5af1e3fd9cb705dcf2af56858e3d9e80855513b5
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2022-03-11 15:29:10 +0700

    Remove login button element instead directly go to login page Removed unneeded libraries New usage using amount of attempts Removal of action driver typing and instead using send keys Remove list of choice to click for pls search and crime, instead shuffle choice within element

--- dankgrinder.py
+++ dankgrinder.py
@@ -5,24 +5,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 import time
-import os
 import sys
-import pickle
 import random
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
-driver.get("http://discord.com")
-
+driver.get("http://discord.com/login")
+driver.maximize_window()
+tries = int(sys.argv[1])
 time.sleep(3)
 
 def login_qr(driver):
-    login_button = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[1]/header[2]/nav/div[2]/a')
-
-    login_button.click()
-
-    qr_scan = int(input("Scan qr, 1 for done 0 for cancel:  "))
+    qr_scan = int(input("Scan qr and go to channel, 1 for done 0 for cancel:  "))
 
     if qr_scan != 1:
         print("Not scanned")
@@ -31,66 +26,66 @@ def login_qr(driver):
 
     print(driver.current_url)
 
-cmd_list = ["pls fish","pls dig","pls hunt"]
-cmd_list2 = ["pls crime","pls search"]
-
-def send(driver,input_element): 
-    for cmd in cmd_list:
-        action = ActionChains(driver)
-        action.move_to_element(input_element).send_keys(cmd).key_down(Keys.ENTER).perform()
-
-    crime(driver,input_element)
-    search(driver,input_element)
-    time.sleep(35)
-    send(driver,input_element)
+def send(driver,input_element):
+    cmd_list = ["pls fish","pls dig","pls hunt"]
+    try:
+        for cmd in cmd_list:
+            print("fish dig hunt")
+            input_element.send_keys(cmd[::-1])
+            input_element.send_keys(Keys.ENTER)
+    except Exception as e:
+        print(getattr(e, 'message', repr(e)))       
 
 def crime(driver,input_element):
-    action = ActionChains(driver)
-        
-    wait_buttons = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.CLASS_NAME, "container-1v9gV9")))
-
-    click_button = ["boredom","identity theft","fraud","murder","tax evasion","shoplifting","vandalism","drug distribution","cyber bullying","arson"]
-          
-    random.shuffle(click_button)
-         
+    print("crime")
+    input_element.click()
     time.sleep(0.8)
-    action.move_to_element(input_element).send_keys("pls crime").key_down(Keys.ENTER).perform()
-    for click in click_button:
+
+    input_element.send_keys("emirc slp")
+    input_element.send_keys(Keys.ENTER)
+    time.sleep(2)
+
+    buttons_to_text = [elements.text for elements in driver.find_elements_by_class_name("children-2XdE_I")]
+    buttons_to_text = buttons_to_text[-1].split("\n")
+    random.shuffle(buttons_to_text)
+
+    for click in buttons_to_text:
         try:
-           wait_button = WebDriverWait(driver,1).until(EC.presence_of_element_located((By.XPATH, f"//button[@class='component-1IAYeC button-38aScr lookFilled-1Gx00P colorBrand-3pXr91 sizeSmall-2cSMqn grow-q77ONN' and contains(.,'{click}')]")))
+           wait_button = WebDriverWait(driver,1).until(EC.presence_of_element_located((By.XPATH, f"//button[@class='component-ifCTxY button-f2h6uQ lookFilled-yCfaCM colorBrand-I6CyqQ sizeSmall-wU2dO- grow-2sR_-F' and contains(.,'{click}')]")))
 
-           elm = driver.find_element_by_xpath(f"//button[@class='component-1IAYeC button-38aScr lookFilled-1Gx00P colorBrand-3pXr91 sizeSmall-2cSMqn grow-q77ONN' and contains(.,'{click}')]")
+           elm = driver.find_element_by_xpath(f"//button[@class='component-ifCTxY button-f2h6uQ lookFilled-yCfaCM colorBrand-I6CyqQ sizeSmall-wU2dO- grow-2sR_-F' and contains(.,'{click}')]")
 
-           ActionChains(driver).move_to_element(elm).click().perform()
-                
-        except:
-           print("Retry...")
-           continue
-        break
+           if elm.is_enabled():
+              elm.click()
+        except Exception as e:
+            print(getattr(e, 'message', repr(e)))
+            continue
+        break       
 
 def search(driver,input_element):
-    action = ActionChains(driver)
-    action.move_to_element(input_element).send_keys("bot").key_down(Keys.ENTER).perform()
-    
-    wait_buttons = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.CLASS_NAME, "container-1v9gV9")))
-
-    click_button = ["mailbox","pocket","bushes","vacuum","glovebox","grass","van","tree","fridge","area51","bus","bank","dog","shoe","washer","mels room","sewer","uber","dumpster","crawlspace","who asked","garage","air"]
-
-    random.shuffle(click_button)
-
+    print("search")
+    input_element.click() 
     time.sleep(0.8)
-    action.move_to_element(input_element).send_keys("pls search").key_down(Keys.ENTER).perform()
 
-    for click in click_button:
+    input_element.send_keys("hcraes slp")
+    input_element.send_keys(Keys.ENTER)
+    time.sleep(1.5)
+
+    buttons_to_text = [elements.text for elements in driver.find_elements_by_class_name("children-2XdE_I")]
+
+    buttons_to_text = buttons_to_text[-1].split("\n")
+    random.shuffle(buttons_to_text)
+
+    for click in buttons_to_text:
         try:
-           wait_button = WebDriverWait(driver,1.2).until(EC.presence_of_element_located((By.XPATH, f"//button[@class='component-1IAYeC button-38aScr lookFilled-1Gx00P colorBrand-3pXr91 sizeSmall-2cSMqn grow-q77ONN' and contains(.,'{click}')]")))
+           wait_button = WebDriverWait(driver,1).until(EC.presence_of_element_located((By.XPATH, f"//button[@class='component-ifCTxY button-f2h6uQ lookFilled-yCfaCM colorBrand-I6CyqQ sizeSmall-wU2dO- grow-2sR_-F' and contains(.,'{click}')]")))
 
-           elm = driver.find_element_by_xpath(f"//button[@class='component-1IAYeC button-38aScr lookFilled-1Gx00P colorBrand-3pXr91 sizeSmall-2cSMqn grow-q77ONN' and contains(.,'{click}')]")
+           elm = driver.find_element_by_xpath(f"//button[@class='component-ifCTxY button-f2h6uQ lookFilled-yCfaCM colorBrand-I6CyqQ sizeSmall-wU2dO- grow-2sR_-F' and contains(.,'{click}')]")
 
-           ActionChains(driver).move_to_element(elm).click().perform()
-                
-        except:
-            print("Retry....")
+           if elm.is_enabled():
+              elm.click()
+        except Exception as e:
+            print(getattr(e, 'message', repr(e)))
             continue
         break
 
@@ -99,5 +94,14 @@ login_qr(driver)
 input_elm = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div[2]/main/form/div[1]/div/div/div[1]/div/div[3]/div[2]/div")))
 
 input_element = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div[2]/main/form/div[1]/div/div/div[1]/div/div[3]/div[2]")
+bg_element = driver.find_element_by_class_name("chatContent-3KubbW")
 
-send(driver,input_element)
+for i in range(tries):
+    send(driver,input_element)
+    time.sleep(2)
+    search(driver,input_element)
+    time.sleep(2)
+    crime(driver,input_element)
+   	
+	print("Cooldown....")
+    time.sleep(35)
